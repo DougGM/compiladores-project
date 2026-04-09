747fb705f94755d16ef6227e7bd5e3fe9c7c0a61
DougGM <douglasmerida35@gmail.com>
2026-04-09 14:00:44 -0600

    Botón desarrolladores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react"
 import Logo from '../assets/imagenes/umg.ico'
 import Users from '../assets/imagenes/users.png'
 
 export const Header = () => {
+  const [mostrar, setMostrar] = useState(false)
+
   return (
-    <div className='bg-neutral-50 flex justify-between h-16 px-3 py-2  rounded-[10px] items-center'>
+    
+    <div className='bg-neutral-50 flex justify-between h-16 px-3 py-2  rounded-[10px] items-center border border-slate-200'>
         <img src={Logo} alt="logo" className='w-10 h-10' />
+
         <h1 className='font-bold text-lg'>PseudoJS</h1>
-        <button className=' bg-neutral-100 size-12 grid place-content-center rounded-lg cursor-pointer hover:bg-neutral-200 '>
-            <img src={Users} alt="colaboradores" className='w-10 h-10 '/>
+
+
+      {/* CONTENEDOR boton desarrolladores */}
+      <div className="relative">
+        <button 
+          onClick={() => setMostrar(!mostrar)} 
+          className='bg-neutral-100 size-12 grid place-content-center rounded-lg cursor-pointer hover:bg-neutral-200'
+        >
+          <img src={Users} alt="colaboradores" className='w-10 h-10'/>
         </button>
+
+        {/* Muestra nombres */}
+        {mostrar && (
+          <div className="absolute right-0 mt-2 w-48 bg-white border border-slate-200 rounded-lg shadow-lg p-3">
+            <p className="font-semibold mb-2">Desarrolladores</p>
+            <p className="text-sm">Douglas Galindo</p>
+            <p className="text-sm">Pedro César Ramos</p>
+          </div>
+        )}
+      </div>
+
     </div>
+
   )
 }   
 
